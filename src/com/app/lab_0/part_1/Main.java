b4908608b2cd4442112cfa59ba50f539abcde027
Wassim Tahraoui <wassim.tahraoui@horizon-tech.tn>
2024-11-12 10:38:34 +0100

    Lab 0

---
+++ src/com/app/lab_0/part_1/Main.java
@@ -0,0 +1,16 @@
+package com.app.lab_0.part_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var n1 = 5;
+        var n2 = 6;
+
+        var sum = 0;
+
+        sum = n1 + n2;
+
+        System.out.println("The result is: " + sum);
+    }
+
+}
